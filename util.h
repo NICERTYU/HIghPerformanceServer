c848652d3c7cef859fa2e5e4d136b2dea58344ba
NICERTYU <148675863+NICERTYU@users.noreply.github.com>
2023-12-21 20:03:51 +0800

    Add files via upload
    工具类 单例

---
+++ util.h
@@ -0,0 +1,23 @@
+//
+// Created by GW on 2023/12/18.
+//
+
+#ifndef HIGHPERFORMANCESERVER_UTIL_H
+#define HIGHPERFORMANCESERVER_UTIL_H
+
+#include <unistd.h>       // for syscall()
+#include <sys/syscall.h>  // for SYS_xxx definitions
+
+//int id = syscall(SYS_thread_selfid); // for mac os x
+#include <csignal>
+#include<sys/syscall.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include <stdio.h>
+namespace superG
+{
+ pid_t GetThreadId();
+ uint32_t GetFiberd();
+}
+
+#endif //HIGHPERFORMANCESERVER_UTIL_H
